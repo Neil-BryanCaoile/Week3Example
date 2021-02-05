c893c930f75939b45d7563f3f66311d42347b26c
Neil-BryanCaoile <caoilen@sheridancollege.ca>
2021-02-05 13:26:44 -0500

    example

---
+++ src/student/Student.java
@@ -0,0 +1,18 @@
+
+package student;
+
+/**
+ *This class +++Insert Description Here+++
+ * This is a sample project using git
+ *@author Neil-Bryan Caoile
+ */
+public class Student {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //learning github local rep,commit , push
+    }
+
+}
